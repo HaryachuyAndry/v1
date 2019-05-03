c4a57213624f9a40b877d0f44c90ffc2aa1396f1
HaryachuyAndry <xghogcui@gmail.com>
2019-05-03 16:47:25 +0300

    Виправив помилку з меню редагування

--- src/components/Header/HeaderButtom/Edit/function.js
+++ src/components/Header/HeaderButtom/Edit/function.js
@@ -2,8 +2,8 @@
 import Edit_style from './../../../Content/Edit/Edit_style.module.css'
 export let HideEditMenu = () =>{
     let x = document.getElementsByClassName(Edit_style.Edit)
-    if (x[0].style.display === 'none'){
-        x[0].style.display = 'block';
+    if (x[0].style.display === 'grid'){
+        x[0].style.display = 'none';
     }
-    else{x[0].style.display = 'none'};
+    else{x[0].style.display = 'grid'};
 }
