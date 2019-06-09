daf600054777ec9609b97652f2c5ee899abb54c8
Dmytro Roik <Dimon@Dmytros-MacBook-Pro.local>
2019-06-09 14:36:43 +0300

    added login page

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import Content_style from './Content.module.css'
+import { Route } from 'react-router-dom';
+import ContentStyle from './Content.module.css';
 import ImgRoom from './room_images/Image';
 import TempRoom from './temp_room/temp_room';
-import {Route} from "react-router-dom";
+
 import ChartsContent from './Charts-content/charts_content';
 import EditSet from './Edit/EditSet';
 import SceneryContainer from './Scenery/sceneryContainer';
 
-const Content = (props) =>{
-    return(
-            <div className={Content_style.Content}>
-                <Route path="/climate" render={() => <TempRoom/>}/>
-                <Route path="/rooms" render={() =><ImgRoom dataRoom={props.dataRoom}/>}/> 
-                <Route path="/charts" render={() =><ChartsContent/>}/> 
-                <Route path="/scenery" render={() =><SceneryContainer/>}/> 
-                <EditSet  Addroom={props.Addroom}/>
-            </div>
-    );  
-}
+const Content = props => (
+  <div className={ContentStyle.Content}>
+    <Route path="/climate" exact render={() => <TempRoom />} />
+    <Route path="/rooms" exact render={() => <ImgRoom dataRoom={props.dataRoom} />} />
+    <Route path="/charts" exact render={() => <ChartsContent />} />
+    <Route path="/scenery" exact render={() => <SceneryContainer />} />
+    <EditSet Addroom={props.Addroom} />
+  </div>
+);
 
-export default Content; 
+export default Content;
