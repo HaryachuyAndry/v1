ae6d45f2b3a27de16819dcd86905283ed2873bcc
Dmytro Roik <Dimon@Dmytros-MacBook-Pro.local>
2019-06-23 18:32:18 +0300

    added bg for navleft bar

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -2,23 +2,24 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import ContentStyle from './Content.module.css';
 import ImgRoom from './room_images/Image';
-import TempRoom from './temp_room/temp_room';
+// import TempRoom from './temp_room/temp_room';
 
-import ChartsContent from './Charts-content/charts_content';
+// import ChartsContent from './Charts-content/charts_content';
 import EditSet from './Edit/EditSet';
-import SceneryContainer from './Scenery/sceneryContainer';
+// import SceneryContainer from './Scenery/sceneryContainer';
 
 const Content = props => (
   <div className={ContentStyle.Content}>
-    <Route path="/climate" exact render={() => <TempRoom />} />
+    <Route path="/climate" exact render={() => <ImgRoom bgUrl="climate_room.png" />} />
     <Route path="/rooms" exact render={() => <ImgRoom bgUrl="rooms_bg.jpg" />} />
     <Route path="/rooms/1" exact render={() => <ImgRoom bgUrl="room1.jpg" />} />
     <Route path="/rooms/2" exact render={() => <ImgRoom bgUrl="room2.jpg" />} />
     <Route path="/rooms/3" exact render={() => <ImgRoom bgUrl="room3.jpg" />} />
     <Route path="/rooms/4" exact render={() => <ImgRoom bgUrl="room4.jpg" />} />
     <Route path="/rooms/5" exact render={() => <ImgRoom bgUrl="room5.jpg" />} />
-    <Route path="/charts" exact render={() => <ChartsContent />} />
-    <Route path="/scenery" exact render={() => <SceneryContainer />} />
+    <Route path="/charts" exact render={() => <ImgRoom bgUrl="charts.png" />} />
+    <Route path="/scenery" exact render={() => <ImgRoom bgUrl="scenario.png" />} />
+    <Route path="/security" exact render={() => <ImgRoom bgUrl="security.png" />} />
     <EditSet Addroom={props.Addroom} />
   </div>
 );
--- src/components/NavBar/control/Control.jsx
+++ src/components/NavBar/control/Control.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Control_style from './Control.module.css';
 
-const Control = () =>{
-    return(
-        <div className={Control_style.control}>
-            Безпека
+const Control = () => (
+  <div className={Control_style.control}>
+          <NavLink to="/security" activeClassName={Control_style.active}>Безпека</NavLink>
         </div>
-    );
-}
+);
 
 export default Control;
